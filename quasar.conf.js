ab8275731c83923e664a9d3e6432118f5ae7be1c
Tuan Pham <tuan@bespokify.com>
2019-10-15 23:45:05 +0700

    Add create word UI

--- quasar.conf.js
+++ quasar.conf.js
@@ -38,7 +38,7 @@ module.exports = function(ctx) {
       //            (not treeshaking Quasar; biggest bundle size; convenient)
       all: "auto",
 
-      components: ["QTable", "QTh", "QTr", "QTd"],
+      components: ["QInput", "QTable", "QTh", "QTr", "QTd"],
       directives: [],
 
       // Quasar plugins
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,24 @@
 <template>
   <q-page>
+    <q-input bottom-slots v-model="newWord" label="New" counter maxlength="12" :dense="dense">
+      <template v-slot:after>
+        <q-btn round dense flat icon="send" @click="addWord" />
+      </template>
+    </q-input>
     <q-table
       title="Words"
       :data="data"
       :columns="columns"
       row-key="name"
       :pagination.sync="pagination"
-    ></q-table>
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="index" :props="props">{{ props.row.index + 1 }}</q-td>
+          <q-td key="word" :props="props">{{ props.row.word }}</q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
@@ -14,15 +26,17 @@
 export default {
   name: "PageIndex",
   data() {
-    this.$axios
-      .get("http://api.volca.tuanpa.wtf/words")
-      .then(res => (this.data = res.data));
+    this.loadWord();
 
     return {
       pagination: {
         rowsPerPage: 3000
       },
       columns: [
+        {
+          name: "index",
+          align: "left"
+        },
         {
           name: "word",
           required: true,
@@ -33,8 +47,34 @@ export default {
           sortable: true
         }
       ],
-      data: []
+      data: [],
+      newWord: ""
     };
+  },
+  methods: {
+    async addWord() {
+      if (this.newWord) {
+        try {
+          await this.$axios.post("http://api.volca.tuanpa.wtf/words", {
+            word: this.newWord
+          });
+
+          this.newWord = "";
+          await this.loadWord();
+        } catch (err) {
+          alert("Exists or something went wrong");
+        }
+      }
+    },
+    async loadWord() {
+      const { data } = await this.$axios.get(
+        "http://api.volca.tuanpa.wtf/words"
+      );
+      this.data = data.map((item, index) => ({
+        ...item,
+        index
+      }));
+    }
   }
 };
 </script>
